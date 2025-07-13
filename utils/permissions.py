1f61c6babce46fd59c8c6249e7891489e202c027
riki4332 <rikikesh.mishra@techyatralabs.com>
2025-07-13 23:47:00 +0545

    add: permissions

---
+++ utils/permissions.py
@@ -0,0 +1,37 @@
+from rest_framework import permissions
+
+
+class IsAdminUser(permissions.BasePermission):
+    def has_permission(self, request, view):
+        print(request.user)
+        if (
+            request.user
+            and request.user.is_authenticated
+            and request.user.is_staff
+            and request.user.role == "admin"
+        ):
+            return True
+        return False
+
+
+class IsStaffUser(permissions.BasePermission):
+    def has_permission(self, request, view):
+        print(request.user)
+        if (
+            request.user
+            and request.user.is_authenticated
+            and request.user.is_staff
+            and request.user.role == "staff"
+        ):
+            return True
+        return False
+
+
+class IsCustomerUser(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(
+            request.user
+            and request.user.is_authenticated
+            and request.user.is_staff
+            and request.user.role == "customer"
+        )
