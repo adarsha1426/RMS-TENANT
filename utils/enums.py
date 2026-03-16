fcb49b1457f3e6e251f8359fae2487364bf22c1d
adarsha1426 <adarshamishra98@gmail.com>
2026-03-16 15:49:10 +0545

    feat:TableStatusChoices been added

--- utils/enums.py
+++ utils/enums.py
@@ -2,6 +2,14 @@ from django.db import models
 
 
 class UserRole(models.TextChoices):
+    """
+    Resturant Owner
+    Manager / admin --> Updated Inventory based on it -->staff
+    Customer --> Reserves Table , Views Menu
+    Superadmin --> Owner of SAAS product who can view all the resturant
+
+    """
+
     ADMIN = "admin", "Admin"
     STAFF = "staff", "Staff"
     CUSTOMER = "customer", "Customer"
@@ -18,3 +26,17 @@ class GenderChoice(models.TextChoices):
     male = "male", "Male"
     female = "female", "Female"
     others = "others", "others"
+
+
+class TableStatusChoices(models.TextChoices):
+    pending = "pending", "Pending"
+    reserved = "reserved", "Reserved"
+    completed = "completed", "Completed"
+    cancelled = "cancelled", "Cancelled"
+
+
+# class MealChoices(models.TextChoices):
+#     breakfast = "breakfast", "BreakFast"
+#     lunch = "lunch", "Lunch"
+#     dinner = "dinner", "Dinner"
+#     all_day = "all", "All Day"
