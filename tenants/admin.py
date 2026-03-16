fe324acb0445d681f63817a5a4d34c52e7d2ded6
adarsha1426 <adarshamishra98@gmail.com>
2026-03-16 15:50:25 +0545

    feat: add list_display to TenantAdminSite and register CustomUser model

--- tenants/admin.py
+++ tenants/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
 from .models import Domain, ResturantTenant
 
+from users.models import CustomUser
+
+from django.contrib.admin import ModelAdmin
+
 
 # Register your models here.
 class TenantAdminSite(admin.AdminSite):
     site_header = "Tenant Admin"
+    list_display = (
+        "name",
+        "domain",
+        "paid_until",
+    )
 
 
 tenant_admin_site = TenantAdminSite(name="tenant_admin_site")
@@ -12,3 +21,4 @@ tenant_admin_site = TenantAdminSite(name="tenant_admin_site")
 # Register models on your custom admin site instance
 tenant_admin_site.register(Domain)
 tenant_admin_site.register(ResturantTenant)
+tenant_admin_site.register(CustomUser)
