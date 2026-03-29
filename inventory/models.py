67ff32e2529ed30991230bddb6dec7cc8f0e1cc6
adarsha1426 <adarshamishra98@gmail.com>
2026-03-29 20:26:19 +0545

    feat: update Inventory and InventoryLog models

--- inventory/models.py
+++ inventory/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from users.models import CustomUser
 from utils.common_model import CommonModel
+from utils.enums import ActionChoices, UnitChoices
 
 
 # Create your models here.
@@ -34,13 +35,12 @@ class Category(CommonModel):
 
 
 class Inventory(CommonModel):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    items = models.CharField(max_length=100)
-    units = models.CharField(max_length=10)  # like gram and kilogram
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    units = models.CharField(max_length=10, choices=UnitChoices)  # like g and kg
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
-    descripton = models.TextField()
+    description = models.TextField()
 
     def __str__(self):
         return (
@@ -49,10 +49,15 @@ class Inventory(CommonModel):
 
 
 class InventoryLog(CommonModel):
-    inventory = models.ForeignKey(Inventory, on_delete=models.CASCADE)
-    used_by = models.ForeignKey(CustomUser, on_delete=models.DO_NOTHING)
+    inventory = models.ForeignKey(Inventory, on_delete=models.PROTECT)
+    action = models.CharField(max_length=10, choices=ActionChoices)
+    used_by = models.ForeignKey(
+        CustomUser, on_delete=models.SET_NULL, blank=True, null=True
+    )
     reason = models.TextField()
-    quantity = models.IntegerField()
+    previous_quantity = models.DecimalField(max_digits=10, decimal_places=2)
+    new_quantity = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        return f"{self.inventory.name} used by {self.used_by} for {self.reason}"
+        username = self.used_by.username if self.used_by else "Unknown User"
+        return f"{username} has  {self.action} for {self.reason}"
