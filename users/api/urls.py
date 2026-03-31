f5d1f65c35e8cb48328f12a4ac3f68c04a59a2bd
adarsha1426 <adarshamishra98@gmail.com>
2026-03-31 19:44:14 +0545

    refactor: restructure token and profile URL patterns for improved organization

--- users/api/urls.py
+++ users/api/urls.py
@@ -15,13 +15,30 @@ from users.api.views import (
 )
 
 
+from django.urls import path, include
+from . import views
+
 urlpatterns = [
-    path("token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path(
+        "token/",
+        include(
+            [
+                path("", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
+                path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+            ]
+        ),
+    ),
     path("login/", LoginView.as_view()),
     path("logout/", LogoutView.as_view()),
+    path(
+        "profile/",
+        include(
+            [
+                path("create/", UserProfileListCreateView.as_view()),
+                path("<int:id>/", UserProfileRetrieveUpdateView.as_view()),
+            ]
+        ),
+    ),
     path("", UserListView.as_view()),
-    path("profile/create", UserProfileListCreateView.as_view()),
     path("create/", UserCreateView.as_view()),
-    path("profile/<int:id>/", UserProfileRetrieveUpdateView.as_view()),
 ]
